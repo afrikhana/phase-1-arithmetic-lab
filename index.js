81ffeea43457d3f803a2f32d3458f01f64610352
Afrikhana <brianmurigi64@gmail.com>
2023-03-25 17:33:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+let num1 = 1;
+let num2 =62;
+let  multiply=num1 * num2
+//=> undefined
+console.log(multiply)
+
+
+Math.random();
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3=8;
+let num4=2;
+let mod=num3 / num4;
+
+var max= [1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,20]
+if (max=20){
+    console.log(max)
+}
+
